9b4b70c026ccdca29571ce3b2ae8040c52483aa5
aneelafatima13 <120780999+aneelafatima13@users.noreply.github.com>
2026-01-28 21:34:50 +0000

    Deploying to gh-pages from @ aneelafatima13/AFPortfolioApp@70e303f3ab8ff48f96f73ff79ec29a56904c6ef4 🚀

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,17 +31,27 @@
   /**
    * Mobile nav toggle
    */
-  const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
+  //const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
 
-  function mobileNavToogle() {
-    document.querySelector('body').classList.toggle('mobile-nav-active');
-    mobileNavToggleBtn.classList.toggle('bi-list');
-    mobileNavToggleBtn.classList.toggle('bi-x');
-  }
-  if (mobileNavToggleBtn) {
+  //function mobileNavToogle() {
+  //  document.querySelector('body').classList.toggle('mobile-nav-active');
+  //  mobileNavToggleBtn.classList.toggle('bi-list');
+  //  mobileNavToggleBtn.classList.toggle('bi-x');
+  //}
+  //if (mobileNavToggleBtn) {
+  //  mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
+  //}
+    /**
+   * Mobile nav toggle
+   */
+    const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
+
+    function mobileNavToogle() {
+        document.querySelector('body').classList.toggle('mobile-nav-active');
+        mobileNavToggleBtn.classList.toggle('bi-list');
+        mobileNavToggleBtn.classList.toggle('bi-x');
+    }
     mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
-  }
-
   /**
    * Hide mobile nav on same-page/hash links
    */
